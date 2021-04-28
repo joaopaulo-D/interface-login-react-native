cbb4b2f8099dbb2015a130eb04a4e765b30404a6
Joao Paulo <joaopaulodeoliveira2003@gmail.com>
2021-04-28 10:29:57 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+
 
 export default function App() {
-  return (
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState(''); 
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <TextInput 
+        style={styles.Input}
+        placeholder="Digite seu nome..."
+        onChangeText={text=>setName(text)}
+      />
+      <TextInput
+        style={styles.Input}
+        placeholder="Digite seu email..."
+        onChangeText={text=>setEmail(text)}
+      />
+      <TextInput
+        style={styles.Input}
+        placeholder="Digite sua senha..."
+        onChangeText={text=>setPassword(text)}
+      />
+      
+      <TouchableOpacity style={styles.cadastro}>
+        <Text style={{ color: 'white' }}>CADASTRA-SE</Text>
+      </TouchableOpacity>
+      
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#191919',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
   },
-});
+  Input: {
+    width: '100%',
+    height: 40,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    paddingLeft: 10,
+    marginBottom: 10
+  },
+  cadastro: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#7b42f5',
+    borderRadius: 20,
+    paddingLeft: 10,
+    marginBottom: 10,
+    justifyContent:'center',
+    alignItems: 'center'
+  }
+})
